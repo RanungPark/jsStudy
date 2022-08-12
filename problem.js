5df883749c759a8efb643d27bb1722d8fb217b3c
RanungPark <ckehfqkr302@naver.com>
2022-08-13 01:32:04 +0900

    22-08-13

--- problem.js
+++ problem.js
@@ -1,10 +1,7 @@
-const num = prompt('입력하실 자연수를 입력해주세요');
-let count = 0;
-
-for(let i=1; i < num; i++){
-  for(let j=1; j < i; j++){
-    if(i % j === 0) count++;
+function pow(x, n) {
+  let num = x;
+  for(let i = 0; i < n; i++) {
+    num = num * x;
   }
-  if(count === 1) alert(i);
-  count = 0;
+  return num;
 }
